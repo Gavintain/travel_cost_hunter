fb1f80929dd73581bdfb177aaa1fa2d8cae200b4
Gavintain <74239444+Gavintain@users.noreply.github.com>
2023-07-27 10:26:07 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from streamlit.report_thread import get_report_ctx
+import SessionState
 from time import sleep
 """
 # Welcome to Streamlit!
@@ -18,19 +20,23 @@ In the meantime, below is an example of what you can do with just a few lines of
 def main():
     st.title("Streamlit 멀티 페이지 앱")
 
-    # 사이드바에서 페이지 선택
-    selected_page = st.sidebar.radio("페이지 선택", ["Home", "About", "Contact"])
+    # 페이지 라우팅 설정
+    session_state = SessionState.get(page="Home")
+    pages = ["Home", "About", "Contact"]
+    page = st.sidebar.selectbox("페이지 선택", pages)
+
+    # 페이지 라우팅 업데이트
+    if session_state.page != page:
+        session_state.page = page
 
     # 각 페이지의 내용을 표시
-    if selected_page == "Home":
+    if session_state.page == "Home":
         st.write("이곳은 홈 페이지입니다.")
-    elif selected_page == "About":
+    elif session_state.page == "About":
         st.write("이곳은 소개 페이지입니다.")
-    elif selected_page == "Contact":
+    elif session_state.page == "Contact":
         st.write("이곳은 연락처 페이지입니다.")
 
 if __name__ == "__main__":
     main()
-
-
 
