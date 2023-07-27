516af28cba45b6972020acffc4102e0fdda596e1
Gavintain <74239444+Gavintain@users.noreply.github.com>
2023-07-27 16:10:11 +0900

    웹 기본작업

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,90 @@
 import streamlit as st
 
 def main():
-    st.title("Streamlit 멀티 페이지 앱")
+    # 사이드바 스타일을 적용하기 위한 CSS 스타일
+    st.markdown(
+        """
+        <style>
+        .sidebar .sidebar-content {
+            background-color: #f8f9fa;
+        }
+        .sidebar .sidebar-item {
+            font-size: 20px;
+            color: #007bff;
+            padding: 10px;
+            margin: 5px 0;
+            border-radius: 10px;
+            cursor: pointer;
+            transition: all 0.3s;
+        }
+        .sidebar .sidebar-item:hover {
+            background-color: #007bff;
+            color: white;
+        }
+        .special-word {
+                color: blue;
+                font-weight: bold;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    page_style = """
+    <style>
+        /* 타이틀을 상단 중앙에 정렬하는 스타일 */
+        .title {
+            text-align: center;
+            margin-top: 20px;
+            color: black;
+            font-size: 40px;
+        }
+        /* 페이지 배경 이미지를 지정하는 스타일 */
+        body {
+            background-image: '/img/undraw_background.png';
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+        }
+    </style>
+    """
+    st.markdown(page_style, unsafe_allow_html=True)
+
+    # 타이틀 표시
+    st.markdown("<h1 class='title'>Travel Cost Hunter</h1>", unsafe_allow_html=True)
 
     # 사이드바에서 페이지 선택
-    selected_page = st.sidebar.radio("페이지 선택", ["Home", "About", "Contact"])
+    selected_page = st.sidebar.selectbox("Menu", ["Home", "Travel Cost Prediction", "DashBoard"])
 
     # 각 페이지의 내용을 표시
     if selected_page == "Home":
-        st.write("이곳은 홈 페이지입니다.")
-    elif selected_page == "About":
-        st.write("이곳은 소개 페이지입니다.")
-    elif selected_page == "Contact":
-        st.write("이곳은 연락처 페이지입니다.")
+
+        """
+
+
+        Travel Cost Hunter에 오신 것을 환영합니다 !
+
+        Travel Cost Hunter는 사용자가 가고자 하는 여행지와 관련된 여행 경비를 쉽고 간단하게 알아볼 수 있는 서비스를 제공합니다.
+
+        현재 Travel Cost Hunter 서비스 범위는 2023년 9월 한국에서 미국으로의 여행 경비로, 해당 항공권과 숙박권 데이터를 기준으로 서비스를 제공합니다.
+
+
+        """
+        
+        st.write("여행 경비를 간단하게 예측하고 싶다면 사이드 메뉴를 통해 <span class='special-word'>Travel Cost Prediction</span> 페이지로 이동하세요!", unsafe_allow_html=True)
+        st.write("항공권, 숙박권 등에 대한 여행 경비 데이터들을 직접 확인하고 싶다면 사이드 메뉴를 통해 <span class='special-word'>DashBoard</span> 페이지로 이동하세요!", unsafe_allow_html=True)
+        st.caption('\n\nTeam I5 ')
+
+
+    elif selected_page == "DashBoard":
+        st.write("이곳은 대시보드 페이지입니다. 대시보드 연결을 기다립니다..")
+        st.caption('\n\nTeam I5 ')
+
+        
+    elif selected_page == "Travel Cost Prediction":
+        st.write("이곳은 여행 경비 예측 모델을 사용해 볼 수 있는 페이지입니다. 모델을 기다립니다..")
+        st.caption('\n\nTeam I5 ')
 
 if __name__ == "__main__":
     main()
