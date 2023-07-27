b41f8a945c8a05201febb3270132005cdd5685bc
Gavintain <74239444+Gavintain@users.noreply.github.com>
2023-07-27 10:24:53 +0900

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-
+from time import sleep
 """
 # Welcome to Streamlit!
 
@@ -15,24 +15,22 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+def main():
+    st.title("Streamlit 멀티 페이지 앱")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # 사이드바에서 페이지 선택
+    selected_page = st.sidebar.radio("페이지 선택", ["Home", "About", "Contact"])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # 각 페이지의 내용을 표시
+    if selected_page == "Home":
+        st.write("이곳은 홈 페이지입니다.")
+    elif selected_page == "About":
+        st.write("이곳은 소개 페이지입니다.")
+    elif selected_page == "Contact":
+        st.write("이곳은 연락처 페이지입니다.")
 
-    points_per_turn = total_points / num_turns
+if __name__ == "__main__":
+    main()
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
